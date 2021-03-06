37f80a7629e8d2a02cc7ceb0ffae34c8c8e6d60e
harryvibhute <hariomvibhute@gmail.com>
2021-03-06 17:25:54 +0530

    Added feature f1

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+int f1();
+
+
 int main()
 {   
    printf("This is master branch");
+   f1();
    return 0;
+}
+
+
+int f1() 
+{
+	printf("This is feature f1");
+	return 0;
 }
 
